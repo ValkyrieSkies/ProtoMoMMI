3b1c0858a1788b844e19760a6a087969acdcffbf
ValkyrieSkies <valkyrianskies@gmail.com>
2023-10-05 21:39:09 +0100

    shitposts

--- protomommi.py
+++ protomommi.py
@@ -65,6 +65,25 @@ async def on_message(message):
             await message.channel.send(outputmsg)
         else:
             await message.channel.send('No players are currently online.')
+            
+    elif message.content.startswith('$manylo'):
+        await message.channel.send('Fuckin\' shitman\'s like manylo are the reason this server struggles with pop. The players aren\'t bad, most of you are decent folk, the Admins aren\'t that bad, most are cool but that fucking SHITHEAD motherfucker is what makes people not enjoy the fucking game anymore.')
+        
+    elif message.content.startswith('$manylo'):
+        await message.channel.send('Fuckin\' shitman\'s like manylo are the reason this server struggles with pop. The players aren\'t bad, most of you are decent folk, the Admins aren\'t that bad, most are cool but that fucking SHITHEAD motherfucker is what makes people not enjoy the fucking game anymore.')
+        
+    elif message.content.startswith('$grape'):
+        await message.channel.send('Shut the fuck up you dumb irrelevant grape holy shit I hate you, you are a waste of space')
+        
+    elif message.content.startswith('$shotgun'):
+        await message.channel.send('I used the shotgun. You know why? Cause the shot gun doesn\'t miss, and unlike the shitty hybrid taser it stops a criminal in their tracks in two hits. Bang, bang, and they\'re fucking done. I use four shots just to make damn sure. Because, once again, I\'m not there to coddle a buncha criminal scum sucking ***, I\'m there to 1) Survive the fucking round. 2) Guard the armory. So you can absolutely get fucked. If I get unbanned, which I won\'t, you can guarantee I will continue to use the shotgun to apprehend criminals. Because it\'s quick, clean and effective as fuck. Why in the seven hells would I fuck around with the disabler shots, which take half a clip just to bring someone down, or with the tazer bolts which are slow as balls, impossible to aim and do about next to jack shit, fuck all. The shotgun is the superior law enforcement weapon. Because it stops crime. And it stops crime by reducing the number of criminals roaming the fucking halls.')
+        
+    elif message.content.startswith('$flarg'):
+        await message.channel.send('I have been: abused, analized, africanized, battered, bummed, buggered, buckbroken, brainscorched, beaten, berated, bludgeoned by big black blocks, cummed, creamed, circumcised, constipated, clapped, dicked, dommmed, edified, fucked, fingered, face-sat, fugged, frazzled, farted on, fumpled, femenized, guzzled, gashed, gaslight, girl bossed, gushed, hemmed, hurt, inseminated, intercoursed, illicted, jacked off, jacked into, jacked onto, killed, kumed, key-stoned, licked, loved, lied to, mushed, ordained, obscured, pooed, pied, pissed, pumped, pillared, quzangled, rectum-fied, raped, ransaked, ravaged, skinned, slaved, slapped, sexoooed, slutified, stuffed, tummied, touched, un-feminized, unloved, vertically-insulted, vroomed, wanked, walled, wrapped, xxxxGAYBOYxxx\'d, yinkled, and zombie-raped.')
+        
+    elif message.content.startswith('$bitch!!!'):
+        await message.channel.send('https://media.tenor.com/QPgNJ7ZEFHsAAAAC/swag-cat.gif')
+    
 
 #create a file named ".env" in the same folder as this and just add a line that's "TOKEN=yourtokenhere"
 client.run(TOKEN)
